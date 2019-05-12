f04bc990188837ac1260485480e7b515f755eef1
Pavlo Rozbytskyi <rozbich15@gmail.com>
2019-05-12 22:36:16 +0200

    implemented ascii symbols output

--- c_tasks/ascii.c
+++ c_tasks/ascii.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
 
-   int main(int argc, char *argv[]) {
-     printf("Hello, world!\n");
-     return 0;
-   }
+int print_ascii(void);
+
+int main(int argc, char *argv[]) {
+    print_ascii();
+
+    return 0;
+}
+
+int print_ascii(void){
+    char *table_header = "Oct Dec Hex Chr\n---------------";
+
+    // clear terminal 
+    clear();
+
+    for (int i = 0; i < 127; i++){
+        if(i == 0 || i == 32 || i == 64 || i == 116 || i == 150){
+            // printing new table header
+            printf("%s\n", table_header);
+
+        }
+        
+        printf("%03o %-3d %-3x ", i, i, i);
+
+        if(isprint(i)){
+            printf("%-3c\n", i);
+        }else{
+            printf("\n");
+        }
+    }
+
+    return 0;
+}

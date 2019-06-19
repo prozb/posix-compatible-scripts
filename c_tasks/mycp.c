1ec8b697d2e374f0ad505c11639f86fad4889aac
= <rozbich15@gmail.com>
2019-06-19 17:06:06 +0200

    implemented recursive  mkdir

--- c_tasks/mycp.c
+++ c_tasks/mycp.c
@@ -1,10 +1,20 @@
+/***************************************
+ *      @author Pavlo Rozbytskyi       *
+ *      @date 19/06/2019               *
+ ***************************************/
 #include <stdio.h>
 #include <sys/stat.h> 
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <libgen.h>
+#include <string.h>
 
 #define BUFFER_SIZE 256
+#define MAX_DIR_LENGTH 256
+
+// recursive creating directories
+int _mkdir(const char *path); 
 
 int main(int argc, char *argv[]){
     if(argc < 3){
@@ -27,6 +37,16 @@ int main(int argc, char *argv[]){
         return 1;
     }
     stat(argv[2], &target);
+
+    // creating intermediate directories
+    char *dir  = dirname(argv[2]);
+    char path[strlen(dir) + 1];
+    strcpy(path, dir); 
+    strcat(path, "/");
+
+    if(_mkdir(path) != 0){
+        return 1;
+    }
 
     // check files equals and do nothing
     if(target.st_dev == source.st_dev && 
@@ -52,6 +72,35 @@ int main(int argc, char *argv[]){
 
     close(tfd);
     close(pfd);
-    
+
+    return 0;
+}
+
+
+int _mkdir(const char *dir){
+    struct stat dir_stat;
+    char buffer [MAX_DIR_LENGTH + 1];
+
+    char *p  = buffer;
+    int size = 0;
+
+    memcpy(buffer, dir, MAX_DIR_LENGTH);
+
+    while(*p != '\0' && size < MAX_DIR_LENGTH + 1){
+        if(*p == '/'){
+            *p = '\0';
+            //creating directory if does not exist
+            if(stat(buffer, &dir_stat) == -1){
+                mkdir(buffer, 0700);
+            }
+
+            if(stat(buffer, &dir_stat) == -1){
+                fprintf(stderr, "Error: cannot create directory \"%s\"\n", buffer);
+                return 1;
+            }
+            *p = '/';
+        }
+        p++;
+    }
     return 0;
 }
